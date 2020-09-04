0292175605c10834a2c58d78cd92212efbd0956c
Takuya Ito <takuya.ito@Takuyas-MacBook-Pro.local>
2020-09-03 21:16:00 -0400

    update

--- scripts/runANNs.py
+++ scripts/runANNs.py
@@ -40,7 +40,7 @@ def runModel(datadir=datadir,practice=True,num_hidden=512,learning_rate=0.0001,t
         print('Creating new training batches')
         if practice:
             print('Creating separate practice and novel batches')
-            TrialInfo = mod.TrialBatchesPracticeNovel(NUM_BATCHES=30000,
+            TrialInfo = mod.TrialBatchesPracticeNovel(NUM_BATCHES=5000,
                                                       NUM_PRACTICE_TRIALS_PER_TASK=10,
                                                       NUM_NOVEL_TRIAlS_PER_TASK=10,
                                                       NUM_INPUT_ELEMENTS=28,
@@ -50,7 +50,7 @@ def runModel(datadir=datadir,practice=True,num_hidden=512,learning_rate=0.0001,t
             TrialInfo.createBatches(condition='novel',nproc=4)
         else:
             print('Creating batches with full task set')
-            TrialInfo = mod.TrialBatchesTrainAll(NUM_BATCHES=30000,
+            TrialInfo = mod.TrialBatchesTrainAll(NUM_BATCHES=5000,
                                                  NUM_TRIALS_PER_TASK=10,
                                                  NUM_INPUT_ELEMENTS=28,
                                                  NUM_OUTPUT_ELEMENTS=4,
@@ -78,15 +78,17 @@ def runModel(datadir=datadir,practice=True,num_hidden=512,learning_rate=0.0001,t
         #### Train practice tasks
         print('Training model on practiced tasks')
         timestart = time.time()
-        mod.batch_training(Network, practice_input_batches,practice_output_batches,cuda=False)  
+        nsamples_viewed1, nbatches_trained1 = mod.batch_training(Network, practice_input_batches,practice_output_batches,cuda=False)  
         timeend = time.time()
         print('Time elapsed using CPU:', timeend-timestart)
 
         print('Training model on novel tasks')
         timestart = time.time()
-        mod.batch_training(Network, novel_input_batches,novel_output_batches,cuda=False)  
+        nsamples_viewed2, nbatches_trained2 = mod.batch_training(Network, novel_input_batches,novel_output_batches,cuda=False)  
         timeend = time.time()
         print('Time elapsed using CPU:', timeend-timestart)
+        print('Total number of batches =', nbatches_trained1 + nbatches_trained2)
+        print('Total number of samples viewed =', nsamples_viewed1 + nsamples_viewed2)
     else:
         print('Loading batches')
         TrialObj = mod.TrialBatchesTrainAll(filename=batchfilename)
@@ -95,20 +97,22 @@ def runModel(datadir=datadir,practice=True,num_hidden=512,learning_rate=0.0001,t
         #### Train practice tasks
         print('Training model on all tasks')
         timestart = time.time()
-        mod.batch_training(Network, input_batches,output_batches,cuda=False)  
+        nsamples_viewed, nbatches_trained = mod.batch_training(Network, input_batches,output_batches,cuda=False)  
         timeend = time.time()
         print('Time elapsed using CPU:', timeend-timestart)
+        print('Total number of batches =', nbatches_trained)
+        print('Total number of samples viewed =', nsamples_viewed)
 
     if save_model is not None:
-        torch.save(Network,datadir + save_model)
+        torch.save(Network,datadir + 'results/model/' + save_model)
 
     #### Save out hidden layer RSM
-    hidden, rsm = analysis.rsa(Network,show=save_hiddenrsm_pdf,savepdf=save_hiddenrsm_pdf)
-    # hidden = hidden.detach().numpy()
-    # input_matrix = input_matrix.detach().numpy()
+    #hidden, rsm = analysis.rsa(Network,show=save_hiddenrsm_pdf,savepdf=save_hiddenrsm_pdf)
+    ## hidden = hidden.detach().numpy()
+    ## input_matrix = input_matrix.detach().numpy()
 
-    # Save out RSM 
-    if save_csv:
-        np.savetxt('ANN1280_HiddenLayerRSM_NoDynamics.csv',rsm)
+    ## Save out RSM 
+    #if save_csv:
+    #    np.savetxt('ANN1280_HiddenLayerRSM_NoDynamics.csv',rsm)
 
     return Network
